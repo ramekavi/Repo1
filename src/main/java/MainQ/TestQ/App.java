5b782b994c3d2d6a171066a182fb3bf6ac15e1c4
srame <srame@DESKTOP-UAOS66I.lan>
2021-06-02 22:46:40 -0400

    commit added 6/2

---
+++ src/main/java/MainQ/TestQ/App.java
@@ -0,0 +1,13 @@
+package MainQ.TestQ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
